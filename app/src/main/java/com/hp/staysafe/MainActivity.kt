db9c2f59c0b5e93408f2559d3d2be12ad3fcab85
pandyah5 <pandyahetav1@gmail.com>
2023-07-25 13:29:05 -0400

    Added an upper bar for app name and sponsor

--- app/src/main/java/com/hp/staysafe/MainActivity.kt
+++ app/src/main/java/com/hp/staysafe/MainActivity.kt
@@ -3,7 +3,18 @@ package com.hp.staysafe
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -11,6 +22,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.hp.staysafe.ui.theme.StaySafeTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +37,68 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    HomeScreen("Bay and College St")
                 }
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun PreviewHomeScreen() {
+    HomeScreen("Bay and College St")
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    StaySafeTheme {
-        Greeting("Android")
+fun HomeScreen(userLoc : String){
+    Column {
+        // An upper bar for app name and sponsor
+        Surface(shadowElevation = 1.dp) {
+            Row (modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Gray),
+                horizontalArrangement = Arrangement.End) {
+                // App Name
+                Text (
+                    text = "App Name",
+                    modifier = Modifier.padding(all = 8.dp),
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Spacer(Modifier.weight(1f))
+
+                // Sponsor Button
+                Button (onClick= {}) {
+                    Image (
+                        painterResource(id = R.drawable.heart_icon),
+                        contentDescription ="Sponsor heart icon",
+                        modifier = Modifier.size(20.dp)
+                    )
+                }
+            }
+        }
+
+        // A rounded text bar for current location with refresh icon
+        Surface {
+            Row {
+                Text (
+                    text = "$userLoc"
+                )
+                Button (onClick= {}) {
+                    Image (
+                        painterResource(id = R.drawable.refresh),
+                        contentDescription ="Refresh icon",
+                        modifier = Modifier.size(20.dp)
+                    )
+                }
+            }
+
+        }
+
+        // Message indicating the safety of current location
+
+        // Tips: Which location too avoid at given time
+
+        // Call 911 button at the bottom of the screen
     }
 }
